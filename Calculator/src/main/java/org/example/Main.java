f73dc38437b48898b7955e4652b99cb5ddab8dc5
GeT <Artem-klevtsov@mail.ru>
2025-06-10 18:38:49 +0300

    add string Array tasks

--- Calculator/src/main/java/org/example/Main.java
+++ Calculator/src/main/java/org/example/Main.java
@@ -1,21 +1,35 @@
 package org.example;
-
 import java.util.Scanner;
 
 /**
  * Created by GeT on 08.06.25.
  * <p>
  * Edited 10.06.25
- *
  * @author GeT
  */
 public class Main {
     /**
+     * Главный метод позволяет запустить 2 разные программы. Калькулятор и нахождение самого длинного слова в массиве
+     */
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter task: 1 - calculator, 2 - string array");
+        int select = scanner.nextInt();
+
+        if (select == 1) {
+            calc();
+        }
+        else if (select == 2) {
+            stringArray();
+        }
+        else System.out.println("Invalid selection");
+    }
+
+    /**
      * В данном методе сделаны базовые функции калькулятора с последующим округлением до 4 знаков
      */
-    public static void main(String[] args) {
+    public static void calc(){
         double result = 0;
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter first digits");
         double firstDigit = scanner.nextDouble();
@@ -46,5 +60,31 @@ public class Main {
         }
 
         System.out.printf("%.4f", result);
+    }
+
+    /**
+     * В этом методе выводится самое длинное слово в массиве.
+     * <p>
+     * Можно улучшить, добавив, что, если 2 слова одной длинны или более, то вывод всех слов. Думаю через вывод циклом обычным print
+     */
+    public static void stringArray(){
+        int quant_letters;
+        int result = 0;
+        int number = 0;
+        System.out.println("Enter quantity elements array");
+        int quantity = scanner.nextInt();
+
+        String[] array = new String[quantity];
+        for (int i = 0; i < quantity; i++) {
+            System.out.println("Enter element " + (i + 1) + " :");
+            array[i] = scanner.next();
+            quant_letters = array[i].length();
+            if (quant_letters > result) {
+                result = quant_letters;
+                number = i;
+            }
+        }
+        scanner.close();
+        System.out.println(array[number]);
     }
 }
