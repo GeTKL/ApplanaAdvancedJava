b929e245d9ef1c95b96b86b0b7e0088155311f33
GeT <Artem-klevtsov@mail.ru>
2025-06-16 21:19:46 +0300

    add try catch exception

--- CalculatorOOP/src/main/java/Main.java
+++ CalculatorOOP/src/main/java/Main.java
@@ -3,13 +3,31 @@ import operation.*;
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * В случае неверного ввода программа завершает свою работу. наверное, лучше было бы сделать циклом на повторный ввод, но решил оставить так, так как задание на обработку исключений.
+     */
     public static void main(String[] args) {
 
-        double num, num2;
-        String op;
-        num = Double.parseDouble(input.inp());
-        num2 = Double.parseDouble(input.inp());
-        op = input.inp();
+        double num = 0, num2 = 0;
+        String op = "";
+
+        try {
+            num = Double.parseDouble(input.inp());
+            num2 = Double.parseDouble(input.inp());
+        }
+        catch (Exception e) {
+            System.out.println("Please enter a valid number");
+            System.exit(0);
+        }
+
+        try {
+            op = input.inp();
+            System.exit(0);
+        }
+        catch (Exception e) {
+            System.out.println("Please enter a valid operation");
+        }
+
         double result = Calculate.calculation(num, num2, op);
         System.out.println(result);
     }
--- CalculatorOOP/src/main/java/operation/division.java
+++ CalculatorOOP/src/main/java/operation/division.java
@@ -6,8 +6,24 @@ public class division extends Operator{
         super(a, b);
     }
 
+    /**
+     * Почему-то выдает бесконечность, почитал, что из-за 2 типов плавающих, решил перекинуть их сначала в int, после вернуть, так получается ошибка
+     * @return Exception и 0 при нуле, либо деление
+     */
     @Override
     public double calculate() {
-        return a / b;
+        try {
+            if ( b == 0)
+            {
+                int aErrorExceptionNull = (int) a;
+                int bErrorExceptionNull = (int) b;
+                return aErrorExceptionNull / bErrorExceptionNull;
+            }
+            return a / b;
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 }
